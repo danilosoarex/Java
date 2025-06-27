8dac0623212430919627eec05d6fefa3af54d67e
Danilo Soares <danilocs.network@gmail.com>
2025-06-26 23:43:36 -0300

    Meus Desafios

---
+++ Meus Desafios/Ex14.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Ex14 {
+
+    // Código que lê o nome de uma pessoa e diz se ela tem o nome "SILVA"
+
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+        System.out.print("Digite o nome da pessoa: ");
+        String nome = leitura.nextLine().trim();
+
+
+    }
+}
