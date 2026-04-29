41c5c5bc72f8d81fbc8f05e5e07178b563b42df5
Danilo Soares <danilocs.network@gmail.com>
2026-04-28 23:41:30 -0300

    Framework Spring

--- Spring/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ Spring/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,13 +1,10 @@
 package br.com.alura.screenmatch;
 
-import br.com.alura.screenmatch.model.DadosEpisodio;
-import br.com.alura.screenmatch.model.DadosSerie;
-import br.com.alura.screenmatch.model.DadosTemporada;
-import br.com.alura.screenmatch.service.ConsumoAPI;
-import br.com.alura.screenmatch.service.ConverteDados;
+import br.com.alura.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -18,19 +15,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoAPI = new ConsumoAPI();
-		var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=game+of_thrones&apikey=279acca5");
-		//System.out.println(json);
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
-
-		json = consumoAPI.obterDados("https://www.omdbapi.com/?t=game+of_thrones&Season=6&apikey=279acca5");
-		DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-		System.out.println(dadosTemporada);
-
-		json = consumoAPI.obterDados("https://www.omdbapi.com/?t=game+of_thrones&Season=6&Episode=10&apikey=279acca5");
-		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
-		System.out.println(dadosEpisodio);
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 }
