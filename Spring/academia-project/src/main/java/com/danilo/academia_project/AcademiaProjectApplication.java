a1cfc30df9228ce0e7b684d842cb4ef1ef767de7
Danilo Soares <danilocs.network@gmail.com>
2026-08-20 19:05:12 -0300

    Start no Academia Projetc

---
+++ Spring/academia-project/src/main/java/com/danilo/academia_project/AcademiaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.danilo.academia_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiaProjectApplication.class, args);
+	}
+
+}
---
+++ Spring/academia-project/src/test/java/com/danilo/academia_project/AcademiaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danilo.academia_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
